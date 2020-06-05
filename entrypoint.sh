0290eb7b3c15d6ece328741984cb705a195497fe
Mason <mason_it@outlook.com>
2020-06-05 17:03:59 +0800

    added generate file function

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+if [[$3 != '']]
+then
+path=$3
+
+else
+path=update.log
+
+if
 
 if [[ $1 == 'tag' ]]
 then
 echo "type is $1"
-log=$(git log $2.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
+git log $2.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S' > path
 
 elif [[ $1 == 'time' ]]
 then
 echo "type is $1"
-log=$(git log --since "$2" --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
+git log --since "$2" --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S' > path
 
 else
 tagName=$(git describe --abbrev=0 --tags)
 echo "tag is $tagName"
-log=$(git log $tagName.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
+git log $tagName.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S' > path
 
 fi
 
-echo "Committed log: $log"
-echo ::set-output name=log::$log
+# echo "Committed log: $log"
+# echo ::set-output name=log::$log
