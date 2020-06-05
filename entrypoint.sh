538cf99b2e215576489c4d9ed05290ee610802ce
Mason <mason_it@outlook.com>
2020-06-05 16:16:42 +0800

    fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-if [[ $1 != '' ]]
+if [[ $1 == 'tag' ]]
 then
-echo "tag is $1"
-log=$(git log $1.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
+echo "type is $1"
+log=$(git log $2.. --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
+
+elif [[ $1 == 'time' ]]
+then
+echo "type is $1"
+log=$(git log --since "$2" --pretty=format:'- %cd %an \n  %s\n \n' --date=format:'%Y-%m-%d %H:%M:%S')
 
 else
 tagName=$(git describe --abbrev=0 --tags)
